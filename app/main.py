67968d9b7127bdf3afda964c27edd5e5cc3cc4da
Anton Konyk <antonkonyk.com>
2024-11-27 17:49:56 +0100

    create class PrintBook(ABC)

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 import json
+from abc import ABC, abstractmethod
 import xml.etree.ElementTree as ET
 
 
@@ -8,13 +9,19 @@ class Book:
         self.content = content
 
 
-class PrintBookConsole:
+class PrintBook(ABC):
+    @abstractmethod
+    def print_book(self, book: Book) -> None:
+        pass
+
+
+class PrintBookConsole(PrintBook):
     def print_book(self, book: Book) -> None:
         print(f"Printing the book: {book.title}...")
         print(book.content)
 
 
-class PrintBookReverse:
+class PrintBookReverse(PrintBook):
     def print_book(self, book: Book) -> None:
         print(f"Printing the book in reverse: {book.title}...")
         print(book.content[::-1])
