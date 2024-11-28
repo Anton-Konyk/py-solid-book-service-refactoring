9d8f8dac66365b3867561bdc859e6af4ffdbebb4
Anton Konyk <antonkonyk.com>
2024-11-28 12:36:24 +0100

    remove Books classes to separate files into book directory

--- app/main.py
+++ app/main.py
@@ -1,67 +1,7 @@
-import json
-from abc import ABC, abstractmethod
-import xml.etree.ElementTree as El_Tree
-
-
-class Book:
-    def __init__(self, title: str, content: str) -> None:
-        self.title = title
-        self.content = content
-
-
-class PrintBook(ABC):
-    @abstractmethod
-    def print_book(self, book: Book) -> None:
-        pass
-
-
-class PrintBookConsole(PrintBook):
-    def print_book(self, book: Book) -> None:
-        print(f"Printing the book: {book.title}...")
-        print(book.content)
-
-
-class PrintBookReverse(PrintBook):
-    def print_book(self, book: Book) -> None:
-        print(f"Printing the book in reverse: {book.title}...")
-        print(book.content[::-1])
-
-
-class DisplayBook(ABC):
-    @abstractmethod
-    def display(self, book: Book) -> None:
-        pass
-
-
-class DisplayBookConsole(DisplayBook):
-    def display(self, book: Book) -> None:
-        print(book.content)
-
-
-class DisplayBookReverse(DisplayBook):
-    def display(self, book: Book) -> None:
-        print(book.content[::-1])
-
-
-class SerializeBook(ABC):
-    @abstractmethod
-    def serialize(self, book: Book) -> str:
-        pass
-
-
-class SerializeBookJson(SerializeBook):
-    def serialize(self, book: Book) -> str:
-        return json.dumps({"title": book.title, "content": book.content})
-
-
-class SerializeBookXml(SerializeBook):
-    def serialize(self, book: Book) -> str:
-        root = El_Tree.Element("book")
-        title = El_Tree.SubElement(root, "title")
-        title.text = book.title
-        content = El_Tree.SubElement(root, "content")
-        content.text = book.content
-        return El_Tree.tostring(root, encoding="unicode")
+from book.book import Book
+from book.book_print import PrintBookConsole, PrintBookReverse
+from book.book_display import DisplayBookConsole, DisplayBookReverse
+from book.book_serialize import SerializeBookJson, SerializeBookXml
 
 
 def main(book: Book, commands: list[tuple[str, str]]) -> None | str:
